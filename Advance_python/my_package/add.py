dea01cc0463a6aedb42a4e323a06722d22a56557
Anjum <anjumnakani.skyscendbs@gmail.com>
2024-03-22 01:17:51 +0700

    Add

---
+++ Advance_python/my_package/add.py
@@ -0,0 +1,13 @@
+
+
+#Todo 9) Create a file 'mod.py' with a class with multiple methods and few member variables
+class MyClass:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):      #Todo 10) Use the package’ only one module.
+        return self.name
+
+    def sum(self,a,b):   #Todo 11) Method of sum module of the package.
+        return a + b
+
---
+++ Advance_python/my_package/multi.py
@@ -0,0 +1,8 @@
+
+#Todo 9) Create a module of multiplication
+
+def multi(x ,y):
+    return x * y
+
+
+
